dd1b495018c0874b059e5ff532142aab01f89633
Vitalii Khodak <vkhodak@luxoft.com>
2018-08-19 21:28:50 +0300

    very first commit with jersey boot skeleton

---
+++ spring-boot-jersey-tutor/src/main/java/org/vit/tutor/jersey/SpringBootJerseyTutorApplication.java
@@ -0,0 +1,12 @@
+package org.vit.tutor.jersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJerseyTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJerseyTutorApplication.class, args);
+	}
+}
