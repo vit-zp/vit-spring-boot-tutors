73ca76d78bd1b19c1c994a8209f5c239a9da14a9
Vitalii Khodak <vkhodak@luxoft.com>
2018-08-19 21:33:44 +0300

    second skeleton for Spring RESTful tutor

---
+++ spring-boot-rest-tutor/src/main/java/org/vit/tutor/rest/SpringBootRestTutorApplication.java
@@ -0,0 +1,12 @@
+package org.vit.tutor.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestTutorApplication.class, args);
+	}
+}
