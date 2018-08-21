c6466ab72bf58be09b07da3d308f5fbfaccc11c9
Vitalii_Khodak <vitalii.khodak@gmail.com>
2018-08-21 10:26:59 +0300

    Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.

--- spring-boot-java-tutor/src/main/java/org/vit/java/tutor/arrays/ArraysUtil.java
+++ spring-boot-java-tutor/src/main/java/org/vit/java/tutor/arrays/ArraysUtil.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 
 public class ArraysUtil {
 
-	public static int countEvenElements(int[] intArr) {
-		return (int)Arrays.stream(intArr)
-				.filter(i -> i % Integer.valueOf(2) == Integer.valueOf(0))
-				.count()
-				;
-		
-	}
+    /*
+    Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+
+     */
+    public static int countEvenElements(int[] intArr) {
+        return (int)Arrays.stream(intArr)
+                .filter(i -> i % Integer.valueOf(2) == Integer.valueOf(0))
+                .count()
+                ;
+
+    }
 
 }
