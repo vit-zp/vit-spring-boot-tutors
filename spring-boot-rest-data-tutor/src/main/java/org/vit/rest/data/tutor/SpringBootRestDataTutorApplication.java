d0fa869660d1a4ae2899719d3594862b63598e8c
Vitalii_Khodak <vitalii.khodak@gmail.com>
2018-08-22 15:31:10 +0300

    first commit to REST-DATA

---
+++ spring-boot-rest-data-tutor/src/main/java/org/vit/rest/data/tutor/SpringBootRestDataTutorApplication.java
@@ -0,0 +1,12 @@
+package org.vit.rest.data.tutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestDataTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestDataTutorApplication.class, args);
+	}
+}
---
+++ spring-boot-rest-data-tutor/src/test/java/org/vit/rest/data/tutor/SpringBootRestDataTutorApplicationTests.java
@@ -0,0 +1,16 @@
+package org.vit.rest.data.tutor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRestDataTutorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
